4d26c39a9c651a40d437d2cfb9f5f9bd1c317150
climbingirl <isvetlanaivanova@yandex.ru>
2025-07-01 15:22:32 +0500

    feat: implement desktop version

---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+import Glide from '@glidejs/glide';
+
+//slider
+
+new Glide('.glide', {
+  type: 'carousel',
+  autoplay: 5000,
+  animationDuration: 1500,
+  perView: 1,
+  gap: 0,
+  hoverpause: true,
+}).mount();
+
+// info-panel
+
+const infoPanel = document.getElementById('info-panel');
+const toggleInfoPanelBtn = document.getElementById('info-panel-btn');
+
+toggleInfoPanelBtn.addEventListener('click', () => {
+  infoPanel.classList.toggle('info-panel--open');
+});
+
+// modal
+
+const reservationBtn = document.getElementById('login-btn');
+const modal = document.getElementById('modal');
+const modalCloseBtn = modal.querySelector('.modal__close-btn');
+const modalOverlay = modal.querySelector('.modal__overlay');
+
+const closeModal = () => {
+  document.body.classList.remove('no-scroll');
+  modal.classList.remove('modal--open');
+};
+const openModal = () => {
+  document.body.classList.add('no-scroll');
+  modal.classList.add('modal--open');
+};
+
+reservationBtn.addEventListener('click', openModal);
+modalCloseBtn.addEventListener('click', closeModal);
+modalOverlay.addEventListener('click', closeModal);
